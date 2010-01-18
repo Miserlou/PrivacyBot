993b0fa4f40db5220efebfeddc8a868cbcdc5c8f
Rich Jones <rich@anomos.info>
2010-01-18 13:53:08 -0500

    jan 18

---
+++ src/org/ale/privacybot/tester.java
@@ -0,0 +1,162 @@
+package org.ale.privacybot;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import android.app.Activity;
+import android.content.res.AssetManager;
+import android.os.Bundle;
+
+public class tester extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        
+		checkInstall();
+		
+		Process p;
+		try {
+			p = Runtime.getRuntime().exec("/data/data/org.ale.privacybot/gpg --homedir /data/data/org.ale.privacybot/gpg/.gpg/ --gen-key");
+			BufferedReader br = new BufferedReader(new InputStreamReader(p.getInputStream()));
+			StringBuffer sb = new StringBuffer();
+			String line;
+			while ((line = br.readLine()) != null) {
+			  sb.append(line).append("\n");
+			}
+			String answer = sb.toString();
+			System.out.println(answer);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		
+		//opposite is outputstreamwriter
+		
+    }
+    
+    public void doCommand(String command, String arg0, String arg1)
+	{
+		try
+		{
+				String fullCmd = command + " " + arg0 + " " + arg1;
+				Process p = Runtime.getRuntime().exec(fullCmd);
+				p.waitFor();
+		}
+		catch (IOException e)
+		{
+			throw new RuntimeException(e.getMessage());
+		}
+		catch (InterruptedException e)
+		{
+			throw new RuntimeException(e.getMessage());
+		}
+	}
+
+	public boolean compareAsset(String assetname)
+	{
+		boolean match = false;
+
+		String destname = "/data/data/org.ale.privacybot/" + assetname;
+		File newasset = new File(destname);
+		try
+		{
+			BufferedInputStream out = new BufferedInputStream(new FileInputStream(newasset));
+			BufferedInputStream in = new BufferedInputStream(this.getAssets().open(assetname));
+			match = true;
+			while(true)
+			{
+				int b = in.read();
+				int c = out.read();
+				if(b != c)
+				{
+					match = false;
+					break;
+				}
+				if(b == -1)
+				{
+					break;
+				}
+			}
+			out.close();
+			in.close();
+		}
+		catch (IOException ex)
+		{
+			match = false;
+		}
+		return match;
+	}
+	
+	public void copyAsset(String assetname)
+	{
+		String destname = "/data/data/org.ale.privacybot/" + assetname;
+		File newasset = new File(destname);
+		try
+		{
+			newasset.createNewFile();
+			BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream(newasset));
+			BufferedInputStream in = new BufferedInputStream(this.getAssets().open(assetname));
+			int b;
+			while((b = in.read()) != -1)
+			{
+				out.write(b);
+			}
+			out.flush();
+			out.close();
+			in.close();
+		}
+		catch (IOException ex)
+		{
+			System.out.println("Failed to copy file '" + assetname + "'.\n");
+		}
+	}
+
+	public void copyFile(String srcname, String destname)
+	{
+		File newasset = new File(destname);
+		File srcfile = new File(srcname);
+		try
+		{
+			newasset.createNewFile();
+			BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream(newasset));
+			BufferedInputStream in = new BufferedInputStream(new FileInputStream(srcfile));
+			int b;
+			while((b = in.read()) != -1)
+			{
+				out.write(b);
+			}
+			out.flush();
+			out.close();
+			in.close();
+		}
+		catch (IOException ex)
+		{
+			System.out.println("Failed to copy file '" + srcname + "' to '" + destname + "'.\n");
+		}
+	}
+	
+	public void checkInstall(){
+		if(!compareAsset("version.txt")){
+			// This is a fucking HACK
+			// Files must be smaller than 1.2M to transfer to /data/data/
+			// EXCEPT if they are FUCKIGN .mp3s
+			// This took me a WHOLE FUCKING DAY of hacking to figure out a solution
+			// GodfuckingDAMMIT
+			// It also takes quite a long time to copy the file so users should be told to wait patiently
+			copyAsset("gpgx.mp3");
+			doCommand("/system/bin/mv", "/data/data/org.ale.privacybot/gpgx.mp3", "/data/data/org.ale.privacybot/gpgx");
+			doCommand("/system/bin/chmod", "777", "/data/data/org.ale.privacybot/gpgx");
+			doCommand("/system/bin/mkdir", "/data/data/org.ale.privacybot/gpg/.gpg/", "");
+			copyAsset("version.txt");
+		}
+	}
+}
