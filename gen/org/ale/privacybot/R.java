f8bacc27999ef4f5f7f28bc86318b2c8fd16dcb4
Rich <tuttle@trashbuntu.(none)>
2010-01-21 02:49:36 -0500

    Better main screen.. doesn't scale though

--- gen/org/ale/privacybot/R.java
+++ gen/org/ale/privacybot/R.java
@@ -15,9 +15,14 @@ public final class R {
         public static final int icon=0x7f020001;
     }
     public static final class id {
+        public static final int email=0x7f050005;
         public static final int install=0x7f050000;
         public static final int install_done=0x7f050001;
+        public static final int keymanager=0x7f050007;
         public static final int out=0x7f050002;
+        public static final int s1=0x7f050004;
+        public static final int s2=0x7f050006;
+        public static final int txt=0x7f050003;
     }
     public static final class layout {
         public static final int installer=0x7f030000;
@@ -25,8 +30,13 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f040001;
+        public static final int blank=0x7f040008;
+        public static final int button_email=0x7f040006;
+        public static final int button_manage_keys=0x7f040007;
+        public static final int button_txt=0x7f040005;
         public static final int hello=0x7f040000;
-        public static final int install_done=0x7f040003;
-        public static final int installing=0x7f040002;
+        public static final int install_done=0x7f040004;
+        public static final int installing=0x7f040003;
+        public static final int pbt=0x7f040002;
     }
 }
