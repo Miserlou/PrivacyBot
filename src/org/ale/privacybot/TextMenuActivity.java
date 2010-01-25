14ac7b3779f70aa8ee3543f2995e6fa2bdd7347d
Rich <tuttle@trashbuntu.(none)>
2010-01-25 15:43:20 -0500

    Whoops, forgot to add the files

---
+++ src/org/ale/privacybot/TextMenuActivity.java
@@ -0,0 +1,50 @@
+package org.ale.privacybot;
+
+import org.openintents.filemanager.FileManagerActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+
+public class TextMenuActivity extends Activity {
+	
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        //no title bar
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        
+        setContentView(R.layout.text_menu);
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        
+    	Button b1 = (Button)findViewById(R.id.send_text);
+    	Button b2 = (Button)findViewById(R.id.send_my_key);
+    	Button b3 = (Button)findViewById(R.id.send_pbt);
+    	
+        b1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+            	startActivity(new Intent(TextMenuActivity.this, KeyInfoListViewActivity.class));
+            }
+        });
+    	
+        b3.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+            	startActivity(new Intent(TextMenuActivity.this, FileManagerActivity.class));
+            }
+        });
+        
+    }
+    
+}
